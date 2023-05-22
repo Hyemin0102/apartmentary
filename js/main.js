8899976db920c18574abe6df4a35a17e1a8121e6
Hyemin0102 <128768462+Hyemin0102@users.noreply.github.com>
2023-05-22 11:50:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -105,11 +105,11 @@ const swiper = new Swiper(".review_right", {
         })
     })
 
-    window.addEventListener('scroll',()=>{
+/*     window.addEventListener('scroll',()=>{
         if(window.scrollY > 40){
             header_mo.style.backgroundColor = "rgb(245, 245, 245)"
         } else { header_mo.style.backgroundColor = "transparent"}
-    })
+    }) */
 
 
 
